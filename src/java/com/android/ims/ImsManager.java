d3356ebe3a912031410b7a79c155726ff58f1db9
Meng Wang <mewan@google.com>
2016-12-07 16:27:15 -0800

    DO NOT MERGE ImsRegistrationListener#registrationChangeFailed
    Bug: 33430556
    Change-Id: Icea36e559f447b14c41a04cd517c12b632bc2c0c

--- src/java/com/android/ims/ImsManager.java
+++ src/java/com/android/ims/ImsManager.java
@@ -1518,7 +1518,7 @@ public class ImsManager {
     /**
      * Adapter class for {@link IImsRegistrationListener}.
      */
-    private class ImsRegistrationListenerProxy extends IImsRegistrationListener.Stub {
+    private static class ImsRegistrationListenerProxy extends IImsRegistrationListener.Stub {
         private int mServiceClass;
         private ImsConnectionStateListener mListener;
 
@@ -1564,6 +1564,7 @@ public class ImsManager {
 
             if (mListener != null) {
                 mListener.onImsConnected();
+                mListener.onImsConnected(imsRadioTech);
             }
         }
 
@@ -1649,6 +1650,16 @@ public class ImsManager {
 
             if (mListener != null) {
                 mListener.registrationAssociatedUriChanged(uris);
+            }
+        }
+
+        @Override
+        public void registrationChangeFailed(int targetAccessTech, ImsReasonInfo imsReasonInfo) {
+            if (DBG) log("registrationChangeFailed :: targetAccessTech=" + targetAccessTech +
+                    ", imsReasonInfo=" + imsReasonInfo);
+
+            if (mListener != null) {
+                mListener.onRegistrationChangeFailed(targetAccessTech, imsReasonInfo);
             }
         }
     }
